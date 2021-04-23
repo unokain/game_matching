913d4d27d23a37113529816da7092e586cc24cee
unokain <unokain@gmail.com>
2021-04-23 18:24:32 +0900

    コメント編集訂正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
    # MiniMagickをincludeすると画像サイズ調整ができるようになる
-  #storage :file # 保存形式の設定。他に使うものとしてはfog形式などがある。
+   # 保存形式の設定。他に使うものとしてはfog形式などがある。
   storage :fog
-  process :resize_to_limit => [50, 50] # 画像サイズの調整
+  process :resize_to_limit => [300, 300] # 画像サイズの調整
   # 画像ファイルの保存先の設定
   # 保存先を指定するには `store_dir` というメソッドに定義します。
   def store_dir
