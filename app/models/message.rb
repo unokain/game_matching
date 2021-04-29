97a3c9d338ccf6057d4345c0cb697410dff6fcd6
unokain <unokain@gmail.com>
2021-04-30 08:52:31 +0900

    チャット機能改善

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ApplicationRecord
   belongs_to :user
   validates_presence_of :body, :conversation_id, :user_id
   def message_time
-    created_at.strftime("%m/%d/%y at %l:%M %p")
+    created_at.strftime("%m/%d at %l:%M %p")
   end
 end
